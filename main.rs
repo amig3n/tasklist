620323043b7b68adbcaf3dde43c47f9c5119a011
amig3n <36228630+amig3n@users.noreply.github.com>
2026-01-09 10:46:46 +0100

    testing code

--- main.rs
+++ main.rs
@@ -10,12 +10,25 @@ fn main() {
 
     let mut task_list = TaskList::new();
 
-    let deadlinedTask = Task::new("Finish this program".to_string(), Some(Utc::now()));
-    task_list.add(deadlinedTask);
-    let notDeadlinedTask = Task::new("Test task without deadline".to_string(), None);
-    task_list.add(notDeadlinedTask);
+    task_list.add(
+        Task::new("Finish this program".to_string(), Some(Utc::now()))
+    );
+    task_list.add(
+        Task::new("Test task without deadline".to_string(), None)
+    );
 
     task_list.show();
+
+    task_list.finish(0);
+    task_list.finish(1);
+    task_list.finish(2);
+
+    task_list.add(
+        Task::new("Add some test code".to_string(), None)
+    );
+
+    task_list.show();
+
    
 
 
